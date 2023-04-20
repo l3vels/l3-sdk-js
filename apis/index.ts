4061fa172d1cfe07d1da3dfa3b3ca15bda17a083
Giga Chkhikvadze <gchkhikvadze@evenergi.com>
2023-04-20 16:17:49 -0500

    fix:  first init

---
+++ apis/index.ts
@@ -0,0 +1,10 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './AssetApi';
+export * from './CollectionApi';
+export * from './ContractApi';
+export * from './GameApi';
+export * from './MintApi';
+export * from './PlayerApi';
+export * from './TransactionApi';
+export * from './UtilitiesApi';
---
+++ models/index.ts
@@ -0,0 +1,17 @@
+/* tslint:disable */
+/* eslint-disable */
+export * from './Asset';
+export * from './AssetsResponseDto';
+export * from './Collection';
+export * from './CreatePlayerDto';
+export * from './MintBatchDto';
+export * from './MintDto';
+export * from './MintDtoAsset';
+export * from './Player';
+export * from './PlayerAsset';
+export * from './Project';
+export * from './SetContractUriDto';
+export * from './SetSaleStatusDto';
+export * from './TokenDto';
+export * from './Transaction';
+export * from './UpdateAssetDto';
