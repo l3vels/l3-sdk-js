3056c18f3a59d497e3c8304cf94546f20e934335
Giga Chkhikvadze <giga.chkhikvadze@email.com>
2023-04-20 16:45:22 -0500

    fix: add open api config

---
+++ openapi/generate_and_push.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERNAME="Giga Chkhikvadze"
+USER_EMAIL="giga.chkhikvadze@email.com"
+
+# Set the OpenAPI Generator options
+CONFIG_FILE="$(pwd)/openapi/typescript-config.json"
+OUTPUT_DIR="generated/sdk-js"
+
+# Generate the client or server code
+npx @openapitools/openapi-generator-cli generate -c "$CONFIG_FILE" -o "$OUTPUT_DIR"
+
+
+# Set Git user
+git config user.name "$USERNAME"
+git config user.email "$USER_EMAIL"
+
+# Add, commit, and push the changes
+git add generated/
+git commit -m "Update generated source code"
+git pull --rebase origin main
+git push origin HEAD
